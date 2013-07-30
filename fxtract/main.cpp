a91a386a7d5b77568eba0f5ee861718a485a10a3
ctSkennerton <c.skennerton@gmail.com>
2013-07-30 20:03:30 +1000

    Initial Commit

---
+++ fxtract/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fxtract
+//
+//  Created by Connor Skennerton on 30/07/13.
+//  Copyright (c) 2013 Connor Skennerton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
