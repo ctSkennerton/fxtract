8b340dcc7819edbafcfab93f231cfa47fea22f74
ctSkennerton <c.skennerton@gmail.com>
2014-06-03 00:13:51 -0700

    Add in new tests looking at header sequences

--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+set -x
 
 diff_output_and_report() {
   diff $1 $2 >/dev/null
   if [ $? != 0 ]; then
-      echo "FAILED test $3!"
+      printf "\x1b[31m%s\x1b[0m\n" "FAILED test $3!"
   else
-      echo "PASSED test $3!"
+      printf "PASSED test $3!\n"
   fi
 
 }
 
+# search for sequences using the different regex engines
 ../fxtract CAAAGGGATTGAGACGCCACTT 1.fa > 1.output.fa
 diff_output_and_report 1.output.fa 1.expected.fa 1a
 
@@ -21,3 +23,15 @@ diff_output_and_report 1.output.fa 1.expected.fa 1c
 
 ../fxtract -P CAAAGGGATTGAGACGCCACTT 1.fa > 1.output.fa
 diff_output_and_report 1.output.fa 1.expected.fa 1d
+
+# search for headers
+../fxtract -H "HISEQ2000:55:C0JRTACXX:2:1101:11128:12710_1:N:0:CTTGTAAT" 1.fa > 2.output.fa
+diff_output_and_report 2.output.fa 2.expected.fa 2a
+../fxtract -HG "HISEQ2000:55:C0JRTACXX:2:1101:11128:12710_1:N:0:CTTGTAAT" 1.fa > 2.output.fa
+diff_output_and_report 2.output.fa 2.expected.fa 2b
+../fxtract -HE "HISEQ2000:55:C0JRTACXX:2:1101:11128:12710_1:N:0:CTTGTAAT" 1.fa > 2.output.fa
+diff_output_and_report 2.output.fa 2.expected.fa 2c
+../fxtract -HP "HISEQ2000:55:C0JRTACXX:2:1101:11128:12710_1:N:0:CTTGTAAT" 1.fa > 2.output.fa
+diff_output_and_report 2.output.fa 2.expected.fa 2d
+../fxtract -HX "HISEQ2000:55:C0JRTACXX:2:1101:11128:12710_1:N:0:CTTGTAAT" 1.fa > 2.output.fa
+diff_output_and_report 2.output.fa 2.expected.fa 2e
